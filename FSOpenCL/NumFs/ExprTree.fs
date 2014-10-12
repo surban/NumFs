0d2cf7abbd13220105276426f43d2acdaf554312
Sebastian Urban <surban@tum.de>
2014-10-12 22:06:54 +0200

    translation groundwork

---
+++ FSOpenCL/NumFs/ExprTree.fs
@@ -0,0 +1,22 @@
+﻿module ExprTree
+
+type Function(name: string, body: Block) =
+    member this.Name = name
+    member this.Body = body
+
+and Var(name: string) =
+    member this.Name = name
+
+and Block = Statement list
+
+and Statement =
+    | VarSet of Var * Expr
+    | Expr of Expr
+
+and Expr =
+    | LiteralInt of int
+    | LiteralFloat of float
+    | LiteralUnit
+    | VarGet of Var
+    | FuncCall of Function * (Expr list)
+    | Ignore of Expr 
--- FSOpenCL/NumFsTest/NDArrayTest.fs
+++ FSOpenCL/NumFsTest/NDArrayTest.fs
@@ -1,7 +1,7 @@
 ﻿open Microsoft.FSharp.Quotations
 
 open NDArray
-open Compiler
+open Translator
 
 let a1 = NDArray.zeros [3; 3]
 a1.|[0;0] <-- 3.14159
@@ -9,27 +9,14 @@ a1.|[1;1] <-- 2.71828
 a1.|[2;2] <-- 1.0
 a1.|[0;2] <-- -1.0
 
-
-type ViewMeta = {a: float}
-let a1m = {a=0.}
+[<ReflectedDefinition>]
+let add x y = 
+    1 |> ignore
+    x + y
 
 [<ReflectedDefinition>]
-let testHost (a: View) = a + 1.
-
-[<ReflectedDefinition>]
-let testTarget (a: View) = 2. * a
-
-[<ReflectedDefinition>]
-let testMeta (a: ViewMeta) = {a=2.*a.a}
-
-registerTargetFunc <@ testHost a1 @> <@ testTarget a1 @> <@ testMeta a1m @>
-registerType typeof<View> typeof<ViewMeta>
+let mul x y = x * y
 
 
-let b = 3
-let input = <@ 3. + testHost a1  @>
-printfn "original expr:     %A" input
-let output = substTargetFuncs input 
-printfn "substituted expr:  %A" output
-
+translate <@ add 1 2 @>
 
